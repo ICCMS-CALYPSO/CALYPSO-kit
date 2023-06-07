b53540de085d9222a34e38ee36b73e10f70eeb25
ixsluo <xiaoshan.luo@outlook.com>
2023-06-07 12:04:12 +0800

    feat(utils): move custom itertools to utils
    Signed-off-by: ixsluo <xiaoshan.luo@outlook.com>

---
+++ calypsokit/utils/itertools.py
@@ -0,0 +1,47 @@
+from itertools import tee
+
+import numpy as np
+
+
+def pairwise(iterable):
+    # pairwise('ABCDEFG') --> AB BC CD DE EF FG
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+class groupby_delta:
+    """groupby delta
+
+    >>> l = [1.0, 1.04, 1.1, 3.1, 3.14, 3.4]
+    >>> for i in groupby_delta(l, 0.1):
+    >>>     print(i)
+    (1.0, 1.04, 1.1)
+    (3.1, 3.14)
+    (3.4,)
+    """
+
+    def __init__(self, iter, delta):
+        self.it = pairwise(iter)
+        self.delta = delta
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        group = []
+        for a, b in self.it:
+            if len(group) == 0:
+                group.append(a)
+                self.final_left = []
+            if b - a < self.delta:
+                group.append(b)
+            else:
+                self.final_left.append(b)
+                return tuple(group)
+        else:
+            if len(group) != 0:
+                return tuple(group)
+            if len(self.final_left) != 0:
+                return (self.final_left.pop(),)
+            raise StopIteration()
--- tests/test_analysis/test_analysis.py
+++ tests/test_analysis/test_analysis.py
@@ -1,9 +1,4 @@
-import sys
 import unittest
-
-import numpy as np
-
-from calypsokit.analysis.sim_group import groupby_delta, pairwise
 
 
 class TestValidity(unittest.TestCase):
@@ -11,14 +6,4 @@ class TestValidity(unittest.TestCase):
 
 
 class TestSimGroup(unittest.TestCase):
-    def test_01_pairwise(self):
-        it = "ABC"
-        it_pair = list(pairwise(it))
-        self.assertTrue((it_pair[0] == ("A", "B")) and (it_pair[1] == ("B", "C")))
-
-    def test_02_groupby_delta(self):
-        it = [0.2, 0.21, 0.22, 0.4, 0.6]
-        it_g = list(groupby_delta(it, 0.1))
-        self.assertTrue(
-            it_g[0] == (0.2, 0.21, 0.22) and (it_g[1] == (0.4,)) and (it_g[2] == (0.6,))
-        )
+    pass
